f39a67ee6e295e6157224fa625c6c50f05ca2d09
LeeYoonJung <imj1017@naver.com>
2022-05-20 17:40:12 +0900

    Init String boot, gradle setting

---
+++ src/main/java/com/yj/momong/MomongApplication.java
@@ -0,0 +1,13 @@
+package com.yj.momong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yj/momong/MomongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.momong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
